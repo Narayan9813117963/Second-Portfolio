5ae8f1defcdfa7f7b3e19002a674f5d5a4d7c2a3
Narayan9813117963 <chaudharynarayan799@gmail.com>
2024-07-22 18:20:26 +0545

    added alt in a tags

--- index.php
+++ index.php
@@ -13,23 +13,23 @@
                     <div class="hero-social d-flex justify-content-between">
                         <ul class="p-0 m-0 d-flex">
                             <li>
-                                <a href="https://www.facebook.com/profile.php?id=100013751371780" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a>
+                                <a href="https://www.facebook.com/profile.php?id=100013751371780" class="d-flex align-items-center justify-content-center" alt="facebook-icon"><i class="bi bi-facebook"></i></a>
                             </li>
                             <li>
-                                <a href="https://www.behance.net/search/projects/web%20developer%20portfolio" class="d-flex align-items-center justify-content-center"><i class="bi bi-behance"></i></a>
+                                <a href="https://www.behance.net/search/projects/web%20developer%20portfolio" class="d-flex align-items-center justify-content-center" alt="behance-icon"><i class="bi bi-behance"></i></a>
                             </li>
                             <li>
-                                <a href="https://api.whatsapp.com/send/?phone=%2B9779813117963&text&type=phone_number&app_absent=0" class="d-flex align-items-center justify-content-center"><i class="bi bi-whatsapp"></i></a>
+                                <a href="https://api.whatsapp.com/send/?phone=%2B9779813117963&text&type=phone_number&app_absent=0" class="d-flex align-items-center justify-content-center" alt="whatsapp-icon"><i class="bi bi-whatsapp"></i></a>
                             </li>
                             <li>
-                                <a href="https://www.linkedin.com/in/narayan-chaudhary-2b9865258/" class="d-flex align-items-center justify-content-center"><i class="bi bi-linkedin"></i></a>
+                                <a href="https://www.linkedin.com/in/narayan-chaudhary-2b9865258/" class="d-flex align-items-center justify-content-center"><i class="bi bi-linkedin" alt="linkedin-icon"></i></a>
                             </li>
                         </ul>
                     </div>
                     <div class="navigation d-lg-flex align-items-center justify-content-center" id="main-nav">
                         <ul class="p-0 m-0 d-flex flex-column">
                             <li>
-                                <a href="#home" class="d-flex align-items-center active"><i><svg width="26" height="24"
+                                <a href="#home" class="d-flex align-items-center active" alt="home-icon"><i><svg width="26" height="24"
                                             viewBox="0 0 26 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path
                                                 d="M21.1665 23.5H4.83317C4.18884 23.5 3.6665 22.9777 3.6665 22.3333V11.8333H0.166504L12.215 0.880097C12.66 0.475555 13.3397 0.475555 13.7847 0.880097L25.8332 11.8333H22.3332V22.3333C22.3332 22.9777 21.8109 23.5 21.1665 23.5ZM14.1665 21.1667H19.9998V9.68371L12.9998 3.32006L5.99984 9.68371V21.1667H11.8332V14.1667H14.1665V21.1667Z"
@@ -38,7 +38,7 @@
                                     </i><span>Home</span></a>
                             </li>
                             <li>
-                                <a href="#about" class="d-flex align-items-center"><i><svg width="26" height="26"
+                                <a href="#about" class="d-flex align-items-center" alt="about-icon"><i><svg width="26" height="26"
                                             viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path
                                                 d="M2.1665 23.8333C2.1665 19.0468 6.0467 15.1667 10.8332 15.1667C15.6197 15.1667 19.4998 19.0468 19.4998 23.8333H17.3332C17.3332 20.2435 14.423 17.3333 10.8332 17.3333C7.24332 17.3333 4.33317 20.2435 4.33317 23.8333H2.1665ZM10.8332 14.0833C7.24192 14.0833 4.33317 11.1746 4.33317 7.58333C4.33317 3.99208 7.24192 1.08333 10.8332 1.08333C14.4244 1.08333 17.3332 3.99208 17.3332 7.58333C17.3332 11.1746 14.4244 14.0833 10.8332 14.0833ZM10.8332 11.9167C13.2273 11.9167 15.1665 9.9775 15.1665 7.58333C15.1665 5.18916 13.2273 3.24999 10.8332 3.24999C8.439 3.24999 6.49984 5.18916 6.49984 7.58333C6.49984 9.9775 8.439 11.9167 10.8332 11.9167ZM19.8072 15.928C22.8196 17.2858 24.9165 20.3147 24.9165 23.8333H22.7498C22.7498 21.1943 21.1772 18.9227 18.9179 17.9044L19.8072 15.928ZM19.0624 3.69764C21.2271 4.59011 22.7498 6.72057 22.7498 9.20833C22.7498 12.3177 20.3711 14.869 17.3332 15.1424V12.9616C19.1713 12.699 20.5832 11.1193 20.5832 9.20833C20.5832 7.71262 19.7182 6.41986 18.4609 5.80271L19.0624 3.69764Z"
@@ -48,7 +48,7 @@
                             </li>
 
                             <li>
-                                <a href="#services" class="d-flex align-items-center"><i><svg width="26" height="26"
+                                <a href="#services" class="d-flex align-items-center" alt="services-icon"><i><svg width="26" height="26"
                                             viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path
                                                 d="M9.13427 3.66676L12.1753 0.625726C12.6309 0.170107 13.3697 0.170107 13.8252 0.625726L16.8663 3.66676H21.167C21.8113 3.66676 22.3336 4.1891 22.3336 4.83343V9.1341L25.3747 12.1752C25.8302 12.6308 25.8302 13.3695 25.3747 13.8251L22.3336 16.8661V21.1668C22.3336 21.8111 21.8113 22.3334 21.167 22.3334H16.8663L13.8252 25.3745C13.3697 25.8301 12.6309 25.8301 12.1753 25.3745L9.13427 22.3334H4.83359C4.18927 22.3334 3.66693 21.8111 3.66693 21.1668V16.8661L0.625893 13.8251C0.170275 13.3695 0.170275 12.6308 0.625893 12.1752L3.66693 9.1341V4.83343C3.66693 4.1891 4.18927 3.66676 4.83359 3.66676H9.13427ZM6.00026 6.00009V10.1006L3.10077 13.0001L6.00026 15.8996V20.0001H10.1008L13.0003 22.8996L15.8998 20.0001H20.0003V15.8996L22.8998 13.0001L20.0003 10.1006V6.00009H15.8998L13.0003 3.1006L10.1008 6.00009H6.00026ZM13.0003 17.6668C10.4229 17.6668 8.33359 15.5774 8.33359 13.0001C8.33359 10.4228 10.4229 8.33343 13.0003 8.33343C15.5776 8.33343 17.667 10.4228 17.667 13.0001C17.667 15.5774 15.5776 17.6668 13.0003 17.6668ZM13.0003 15.3335C14.289 15.3335 15.3336 14.2888 15.3336 13.0001C15.3336 11.7114 14.289 10.6668 13.0003 10.6668C11.7116 10.6668 10.667 11.7114 10.667 13.0001C10.667 14.2888 11.7116 15.3335 13.0003 15.3335Z"
@@ -57,7 +57,7 @@
                                     </i><span>Services</span></a>
                             </li>
                             <li>
-                                <a href="#portfolio" class="d-flex align-items-center"><i><svg width="25" height="24"
+                                <a href="#portfolio" class="d-flex align-items-center" alt="portfolio-icon"><i><svg width="25" height="24"
                                             viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path
                                                 d="M6.66683 4.83333V1.33333C6.66683 0.689004 7.18917 0.166664 7.8335 0.166664H17.1668C17.8112 0.166664 18.3335 0.689004 18.3335 1.33333V4.83333H23.0002C23.6445 4.83333 24.1668 5.35567 24.1668 6V22.3333C24.1668 22.9777 23.6445 23.5 23.0002 23.5H2.00016C1.35584 23.5 0.833496 22.9777 0.833496 22.3333V6C0.833496 5.35567 1.35584 4.83333 2.00016 4.83333H6.66683ZM3.16683 17.6667V21.1667H21.8335V17.6667H3.16683ZM3.16683 15.3333H21.8335V7.16666H3.16683V15.3333ZM9.00016 2.5V4.83333H16.0002V2.5H9.00016ZM11.3335 11.8333H13.6668V14.1667H11.3335V11.8333Z"
@@ -66,7 +66,7 @@
                                     </i><span>Portfolio</span></a>
                             </li>
                             <li>
-                                <a href="#contact" class="d-flex align-items-center"><i><svg width="25" height="25"
+                                <a href="#contact" class="d-flex align-items-center" alt="contact-icon"><i><svg width="25" height="25"
                                             viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path
                                                 d="M24.1668 17.8359C24.1657 21.1853 21.812 23.9847 18.6686 24.6719L17.9242 22.4389C19.3289 22.2096 20.5227 21.3533 21.2092 20.1667H18.3335C17.0448 20.1667 16.0002 19.122 16.0002 17.8333V13.1667C16.0002 11.878 17.0448 10.8333 18.3335 10.8333H21.7613C21.1872 6.22931 17.2597 2.66667 12.5002 2.66667C7.74061 2.66667 3.81315 6.22931 3.23903 10.8333H6.66683C7.95549 10.8333 9.00016 11.878 9.00016 13.1667V17.8333C9.00016 19.122 7.95549 20.1667 6.66683 20.1667H3.16683C1.87816 20.1667 0.833496 19.122 0.833496 17.8333V12C0.833496 5.55668 6.05684 0.333334 12.5002 0.333334C18.9434 0.333334 24.1668 5.55668 24.1668 12V13.1652V13.1667V17.8333V17.8359Z"
@@ -99,7 +99,7 @@
                     <div class="row">
                         <div class="col-lg-5 ps-0">
                             <figure class="about-img mx-auto mx-lg-0">
-                                <img src="images/about-img.png" alt="about-img">
+                                <img src="images/about-img.webp" alt="about-img">
                             </figure>
                         </div>
                         <div class="col-lg-7 pe-0 d-flex">
@@ -126,8 +126,8 @@
                                     </div>
                                 </div>
                                 <div class="about-btn d-flex gap-3 justify-content-center justify-content-lg-start">
-                                    <a href="images/Narayan-Chaudhary-cv (updated).pdf"
-                                        class="primary-btn d-block">Download CV</a>
+                                    <a href="images/cv.pdf"
+                                        class="primary-btn d-block" alt="cv">Download CV</a>
                                     <a href="#contact" class="secondary-btn hire-btn d-block">Hire me</a>
                                 </div>
                             </div>
@@ -286,11 +286,11 @@
                         <span class="subtitle">Collection of My Projects</span>
                         <div class="row portfolio-wrapper row-gap-4">
                             <div class="col-12 col-sm-6 col-sm-6 col-lg-4">
-                                <a href="https://basnyatbrew.onrender.com/" class="text-center">
+                                <a href="https://basnyatbrew.onrender.com/" class="text-center" alt="project-thumbnail">
                                     <div
                                         class="portfolio-card d-flex flex-column align-items-center justify-content-center position-relative">
                                         <figure class="service-thumb">
-                                            <img src="images/coffee-shop-1.png" alt="">
+                                            <img src="images/coffee-shop-1.webp" alt="">
                                         </figure>
                                         <div class="card-overlay position-absolute"></div>
                                         <div class="card-btn position-absolute">
@@ -301,11 +301,11 @@
                                 </a>
                             </div>
                             <div class="col-12 col-sm-6 col-lg-4">
-                                <a href="https://basnyatbrew.onrender.com/" class="text-center">
+                                <a href="https://basnyatbrew.onrender.com/" class="text-center" alt="project-thumbnail">
                                     <div
                                         class="portfolio-card d-flex flex-column align-items-center justify-content-center position-relative">
                                         <figure class="service-thumb">
-                                            <img src="images/coffee-shop-1.png" alt="">
+                                            <img src="images/coffee-shop-1.webp" alt="">
                                         </figure>
                                         <div class="card-overlay position-absolute"></div>
                                         <div class="card-btn position-absolute">
@@ -316,11 +316,11 @@
                                 </a>
                             </div>
                             <div class="col-12 col-sm-6 col-lg-4">
-                                <a href="https://basnyatbrew.onrender.com/" class="text-center">
+                                <a href="https://basnyatbrew.onrender.com/" class="text-center" alt="project-thumbnail">
                                     <div
                                         class="portfolio-card d-flex flex-column align-items-center justify-content-center position-relative">
                                         <figure class="service-thumb">
-                                            <img src="images/coffee-shop-1.png" alt="">
+                                            <img src="images/coffee-shop-1.webp" alt="">
                                         </figure>
                                         <div class="card-overlay position-absolute"></div>
                                         <div class="card-btn position-absolute">
@@ -364,7 +364,7 @@
                         <div class="row contact-info-wrapper row-gap-4">
                             <div class="col-12 col-md-6 col-xxl-5">
                                 <div class="info-card">
-                                    <a href="mailto:Chaudharynarayan799@gmail.com" class="d-flex">
+                                    <a href="mailto:Chaudharynarayan799@gmail.com" class="d-flex" alt="mail-card">
                                         <span class="info-icon position-relative"><i class="bi bi-envelope"></i></span>
                                         <div class="contact-detail">
                                             <h3>Email</h3>
@@ -375,7 +375,7 @@
                             </div>
                             <div class="col-12 col-md-6 col-xxl-3">
                                 <div class="info-card">
-                                    <a href="tel: +9779813117963" class="d-flex">
+                                    <a href="tel: +9779813117963" class="d-flex" alt="phone-card">
                                         <span class="info-icon position-relative"><i class="bi bi-telephone"></i></span>
                                         <div class="contact-detail">
                                             <h3>Phone</h3>
@@ -387,7 +387,7 @@
                             <div class="col-12 col-md-6 col-xxl-4">
                                 <div class="info-card">
                                     <a href="https://www.google.com/maps/search/?api=1&query=27.721042,85.271010"
-                                        class="d-flex">
+                                        class="d-flex" alt="address-card">
                                         <span class="info-icon position-relative"><i
                                                 class="bi bi-geo-alt-fill"></i></span>
                                         <div class="contact-detail">
@@ -437,7 +437,7 @@
             <div class="offcanvas-body">
                 <div class="offcanvas-contents">
                     <ul class="d-flex flex-column p-0">
-                        <li class="text-center"><a class="py-3 d-block w-100 offcanvas-link" href="#">Home</a></li>
+                        <li class="text-center"><a class="py-3 d-block w-100 offcanvas-link" href="../index.php">Home</a></li>
                         <li class="text-center"><a class="py-3 d-block w-100 offcanvas-link" href="#about">About</a>
                         </li>
                         <li class="text-center"><a class="py-3 d-block w-100 offcanvas-link"
